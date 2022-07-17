82a59207466d6797c80f06c915e81b89e1b02df8
ockie1729 <you_oki1@yahoo.co.jp>
2022-07-18 05:55:50 +0900

    hack: point->string

--- isuumo/webapp/go/main.go
+++ isuumo/webapp/go/main.go
@@ -71,6 +71,7 @@ type Estate struct {
 	DoorWidth   int64   `db:"door_width" json:"doorWidth"`
 	Features    string  `db:"features" json:"features"`
 	Popularity  int64   `db:"popularity" json:"-"`
+	Latlon      string  `db:"latlon" json:"-"`
 }
 
 //EstateSearchResponse estate/searchへのレスポンスの形式
