1d9e2d5afbe6786f3a53a4cd0dff24f2b43a57b9
ockie1729 <you_oki1@yahoo.co.jp>
2022-07-17 21:58:32 +0900

    Add rent_id_index to estate table

--- isuumo/webapp/mysql/db/0_Schema.sql
+++ isuumo/webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    INDEX       rent_id_index       (rent, id)
 );
 
 CREATE TABLE isuumo.chair
